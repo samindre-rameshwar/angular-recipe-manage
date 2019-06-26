12e2b9a9d74cddb76d0af4d2e3304287e9926a77
Ram Samindre <samindre.rameshwar@gmail.com>
2019-06-26 10:20:08 +0530

    Template driven form - Shopping list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
 import { Ingredient } from '../shared/ingredients.modal';
 import { Subject } from 'rxjs';
+import { ThrowStmt } from '@angular/compiler';
 
 export class ShoppingListService {
     ingredientChanged = new Subject<Ingredient[]>();
-
+    startedEditing = new Subject<number>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomato', 15),
@@ -14,16 +15,27 @@ export class ShoppingListService {
         return this.ingredients.slice();
     }
 
+    getIngredientsById(index:number){
+         return this.ingredients[index];
+    }
+
     addIngredient(ingredient:Ingredient){
         this.ingredients.push(ingredient);
         this.ingredientChanged.next(this.ingredients.slice());
     }
 
-    addMultiIngredients(ingredients:Ingredient[]){
-        // for(let ingredient of ingredients){
-        //     this.addIngredient(ingredient);
-        // } 
+    addMultiIngredients(ingredients:Ingredient[]){ 
         this.ingredients.push(...ingredients);
+        this.ingredientChanged.next(this.ingredients.slice());
+    }
+
+    updateIngredient(index:number,newIngredients){
+        this.ingredients[index] = newIngredients;
+        this.ingredientChanged.next(this.ingredients.slice());
+    }
+
+    deleteIngedient(index:number){
+        this.ingredients.splice(index,1);
         this.ingredientChanged.next(this.ingredients.slice());
     }
 }
